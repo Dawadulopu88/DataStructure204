c57009eebee5fc4da5ec31663401923451bf3d0d
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2024-02-01 10:28:04 +0600

    Add files via upload

--- Task1.c
+++ Task1.c
@@ -1,33 +1,69 @@
 #include<stdio.h>
-int main(){
+int main()
+{
     int n;
-printf("Enter the size of array:");
-scanf("%d",&n);
-int a[n];
-printf("Enter the elements:");
-int i;
-for (i=0;i<n;i++){
-   scanf("%d",&a[i]);
-}
-printf("The final array is:\n");
-for(i=0;i<n;i++){
-    printf("%d\n",a[i]);
-}
-int key;
-printf("Searching Value:");
-scanf("%d",&key);
-int k=0;
-int loc = -1;
-while(k<=n-1){
-    if(key==a[k]){
-        loc=k;
-        printf("Found at Index Number:%d",loc);
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements:");
+    int i;
+    for (i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
     }
-    k++;
-}
-    if(loc==-1){
-        printf("Value Not found in the array");
+    printf("The array is:\n");
+    for(i=0; i<n; i++)
+    {
+        printf("%d\n",a[i]);
     }
 
-return 0;
+    for(int i=0; i<n; i++)
+    {
+        int temp=a[i];
+        int j;
+        j=i-1;
+        while(temp<a[j]&&j>=0)
+        {
+            a[j+1]=a[j];
+            j--;
+
+        }
+        a[j+1]=temp;
+    }
+    printf("The sorted list:\n");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    while(1){
+    int key;
+    printf("Enter what you want to search:");
+    scanf("%d",&key);
+    int low=0;
+    int loc=-1;
+    int high=n-1;
+    while(low<=high)
+    {
+        int pos=low+((key-a[low])*(high-low))/(a[high]-a[low]);
+        if(a[pos]==key)
+        {
+            loc=pos;
+            printf("Found at index %d\n",loc);
+            break;
+        }
+        if(a[pos]<key)
+        {
+            low=pos+1;
+        }
+        else
+        {
+            high=pos-1;
+        }
+    }
+    if(loc==-1)
+    {
+        printf("Not found\n");
+    }
+    }
+    return 0;
 }
